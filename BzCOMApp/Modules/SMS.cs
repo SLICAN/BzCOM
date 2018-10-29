aa6d393465aaae8d1aba20ab1156b7cf22f9746d
AdriannaPiszcz <adrianna.piszcz@vp.pl>
2018-10-29 19:09:29 +0100

    123

---
+++ BzCOMApp/Modules/SMS.cs
@@ -0,0 +1,184 @@
+﻿namespace ChatTest
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class XCTIPSMS
+    {
+        private XCTIPSMSRegister_REQ[] register_REQField;
+
+        private XCTIPSMSUnregister_REQ[] unregister_REQField;
+
+        private XCTIPSMSSend_REQ[] send_REQField;
+
+        private XCTIPSMSReceive_EV[] receive_EVField;
+
+        private XCTIPSMSAnswer[] answerField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Register_REQ", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XCTIPSMSRegister_REQ[] Register_REQ { get => register_REQField; set => register_REQField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Unregister_REQ", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XCTIPSMSUnregister_REQ[] Unregister_REQ { get => unregister_REQField; set => unregister_REQField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Send_REQ", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XCTIPSMSSend_REQ[] Send_REQ { get => send_REQField; set => send_REQField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Receive_EV", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XCTIPSMSReceive_EV[] Receive_EV { get => receive_EVField; set => receive_EVField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Answer", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public XCTIPSMSAnswer[] Answer { get => answerField; set => answerField = value; }
+        
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class XCTIPSMSRegister_REQ
+    {
+        private string cIdField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("CId", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string CId { get => cIdField; set => cIdField = value; }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class XCTIPSMSUnregister_REQ
+    {
+        private string cIdField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("CId", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string CId { get => cIdField; set => cIdField = value; }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class XCTIPSMSReceive_EV
+    {
+        private string recvTimeField;
+
+        private string numberField;
+
+        private string typeField;
+
+        private string textField;
+
+        private string userDataField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("RecvTime", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string RecvTime { get => recvTimeField; set => recvTimeField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Number", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Number { get => numberField; set => numberField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Type", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Type { get => typeField; set => typeField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Text", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Text { get => textField; set => textField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("UserData", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string UserData { get => userDataField; set => userDataField = value; }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class XCTIPSMSSend_REQ
+    {
+        private string cIdField;
+
+        private string sMSIdField;
+
+        private string numberField;
+
+        private string textField;
+
+        private string typeField;
+
+        private string dontBufferField;
+
+        private string userDataField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("CId", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string CId { get => cIdField; set => cIdField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("SMSId", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string SMSId { get => sMSIdField; set => sMSIdField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Number", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Number { get => numberField; set => numberField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Text", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Text { get => textField; set => textField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("DontBuffer", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string DontBuffer { get => dontBufferField; set => dontBufferField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("UserData", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string UserData { get => userDataField; set => userDataField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Type", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Type { get => typeField; set => typeField = value; }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class XCTIPSMSAnswer
+    {
+        private string cIdField;
+
+        private string errorField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("CId", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string CId { get => cIdField; set => cIdField = value; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Error", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public string Error { get => errorField; set => errorField = value; }
+    }
+
+}
---
+++ BzCOMApp/Parsers/XMLInterpreter.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatTest
+{
+    public class XMLInterpreter
+    {
+        public List<User> UserInfo = new List<User>();
+
+        private Connection connection;
+
+        public string Error { get; set; }
+
+        private Logger logger = new Logger();
+
+        public XMLInterpreter(Connection connection)
+        {
+            this.connection = connection;
+        }
+
+        /// <summary>
+        /// Funkcja zwraca stosowną odpowiedź na polecenie zalogowania
+        /// </summary>
+        /// <returns></returns>
+        public string LogIn(XCTIP packet)
+        {
+            if (!LogError(packet))
+                return CurrentUserInfo();
+            return null;
+        }
+
+        
+
+        /// <summary>
+        /// Funkcja zwraca informacje o zalogowanym użytkowniku
+        /// </summary>
+        /// <returns></returns>
+        public string CurrentUserInfo()
+        {
+            string answer = null;
+            lock (TrafficController.asyncData)
+            {
+                for (int i = 0; i < TrafficController.asyncData.Count;)
+                {
+                    var packet = TrafficController.asyncData[i];
+                    if (packet.LogItems != null && packet.LogItems[0].LogInfo_ANS != null)
+                    {
+                        answer = "Witaj! Twój numer telefonu to: " + packet.LogItems[0].LogInfo_ANS[0].Number;
+                        TrafficController.asyncData.RemoveAt(i);
+                    }
+                    else
+                        i++;
+                }
+            }
+            return answer;
+        }
+
+        public void GetBook()
+        {
+            if (connection.State == State.LoggedIn)
+            {
+                lock (TrafficController.asyncData)
+                {
+                    for (int i = 0; i < TrafficController.asyncData.Count;)
+                    {
+                        var packet = TrafficController.asyncData[i];
+                        if (packet.SyncItems == null || packet.SyncItems[0].Records_ANS == null)
+                        {
+                            i++;
+                            continue;
+                        }
+                        foreach (var row in packet.SyncItems[0].Records_ANS[0].Row)
+                        {
+                            User user = new User();
+                            if (row.Contact != null && row.Contact[0].Name != null)
+                            {
+                                user.UserId = row.Contact[0].IdExtNo;
+                                user.UserName = row.Contact[0].Name;
+                                user.ContactId = row.Contact[0].ContactId;
+                                /// tutaj foreach po phone i pola phone number i phone desc jako tablica słowników, tuple czy cokolwiek
+                                if (row.Contact[0].Phone != null)
+                                {
+                                    user.PhoneNumber = row.Contact[0].Phone[0].Number;
+                                    user.PhoneDesc = row.Contact[0].Phone[0].Comment;
+                                }
+
+                                user.UserNumber = row.Contact[0].ExtNo;
+                                UserInfo.Add(user);
+                            }
+                        }
+                        TrafficController.asyncData.RemoveAt(i);
+                    }
+                }
+            }
+            else
+            {
+                Error = "Najpierw musisz ustanowić połączenie z serwerem i zalogować się";
+                logger.Debug($"Error:\n{Error}\n");
+            }
+        }
+
+        /// <summary>
+        /// Pobiera wszystkie statusy - metoda startowa
+        /// </summary>
+        /// <returns> </returns>
+        public List<User> GetStatus()
+        {
+            List<User> users = new List<User>();
+            lock (TrafficController.asyncData)
+            {
+                for (int i = 0; i < TrafficController.asyncData.Count;)
+                {
+                    var packet = TrafficController.asyncData[i];
+                    if (packet.StatusItems != null && packet.StatusItems[0].Refresh_EV != null)
+                    {
+                        User user = new User
+                        {
+                            UserId = packet.StatusItems[0].Refresh_EV[0].Id
+                        };
+                        foreach (var el in UserInfo)
+                        {
+                            if (el.UserId == user.UserId)
+                            {
+                                user.UserName = el.UserName;
+                                user.UserNumber = el.UserNumber;
+                            }
+                        }
+                        if (packet.StatusItems[0].Refresh_EV[0].AppState != null)
+                            user.UserState = (Status)Enum.Parse(typeof(Status), packet.StatusItems[0].Refresh_EV[0].AppState);
+                        else
+                            user.UserState = Status.UNAVAILABLE;
+                        if (packet.StatusItems[0].Refresh_EV[0].AppInfo != null)
+                            user.UserDesc = packet.StatusItems[0].Refresh_EV[0].AppInfo;
+                        users.Add(user);
+                        TrafficController.asyncData.RemoveAt(i);
+                    }
+                    else
+                        i++;
+                }
+
+            }
+            UserInfo = users;
+            return users;
+        }
+
+
+        /// <summary>
+        /// Pobiera aktualizacje statusów
+        /// </summary>
+        /// <returns></returns>
+        public List<User> GetChangedStatus()
+        {
+            List<User> users = new List<User>();
+            User user = new User();
+            lock (TrafficController.asyncData)
+            {
+
+                for (int i = 0; i < TrafficController.asyncData.Count;)
+                {
+                    var packet = TrafficController.asyncData[i];
+
+                    if (packet.StatusItems != null && packet.StatusItems[0].Change_EV != null)
+                    {
+                        user.UserId = packet.StatusItems[0].Change_EV[0].Id;
+                        foreach (var el in UserInfo)
+                        {
+                            if (el.UserId == user.UserId)
+                                user.UserName = el.UserName;
+                        }
+                        if (packet.StatusItems[0].Change_EV[0].AppState != null)
+                        {
+                            user.UserState = (Status)Enum.Parse(typeof(Status), packet.StatusItems[0].Change_EV[0].AppState);
+                            packet.StatusItems[0].Change_EV[0].AppState = null;
+                        }
+
+                        if (packet.StatusItems[0].Change_EV[0].AppInfo != null)
+                        {
+                            user.UserDesc = packet.StatusItems[0].Change_EV[0].AppInfo;
+                            packet.StatusItems[0].Change_EV[0].AppInfo = null;
+                        }
+                        users.Add(user);
+                        TrafficController.asyncData.RemoveAt(i);
+                    }
+                    else
+                        i++;
+                }
+            }
+            return users;
+        }
+
+        public List<Message> GetMessageRecords_EV(List<XCTIP> packets)
+        {
+            Message message = new Message();
+            List<Message> messages = new List<Message>();
+            for (int i = 0; i < TrafficController.asyncData.Count;)
+            {
+                var packet = TrafficController.asyncData[i];
+                if (packet.SyncItems != null && packet.SyncItems[0].Records_EV != null && packet.SyncItems[0].Records_EV[0].Row != null)
+                {
+                    foreach (var item in packet.SyncItems[0].Records_EV[0].Row)
+                    {
+                        message.DateTime = Convert.ToDateTime(item.HistoryMsg[0].Date);
+                        message.Number = Convert.ToInt32(item.HistoryMsg[0].Number);
+                        message.Text = item.HistoryMsg[0].Text;
+                        messages.Add(message);
+                    }
+                    TrafficController.asyncData.RemoveAt(i);
+                }
+                else
+                    i++;
+            }
+            return messages;
+        }
+
+        public List<Message> GetMessageRecords_ANS()
+        {
+
+            List<Message> messages = new List<Message>();
+            lock (TrafficController.asyncData)
+            {
+                for (int i = 0; i < TrafficController.asyncData.Count;)
+                {
+                    var packet = TrafficController.asyncData[i];
+                    if (packet.SyncItems == null || packet.SyncItems[0].Records_ANS == null || packet.SyncItems[0].Records_ANS[0].Row == null)
+                    {
+                        i++;
+                        continue;
+                    }
+                    foreach (var item in packet.SyncItems[0].Records_ANS[0].Row)
+                    {
+                        Message message = new Message();
+                        if (item.HistoryMsg == null)
+                            continue;
+
+                        message.DateTime = Convert.ToDateTime(item.HistoryMsg[0].Date);
+                        message.Number = Convert.ToInt32(item.HistoryMsg[0].Number);
+                        message.Text = item.HistoryMsg[0].Text;
+                        messages.Add(message);
+                        TrafficController.asyncData.RemoveAt(i);
+                    }
+                }
+            }
+            return messages;
+        }
+
+        public Message GetSMSReceive_EV()
+        {
+            Message message = new Message();
+            lock (TrafficController.asyncData)
+            {
+                for (int i = 0; i < TrafficController.asyncData.Count;)
+                {
+                    var packet = TrafficController.asyncData[i];
+                    if (packet.SMSItems == null || packet.SMSItems[0].Receive_EV == null)
+                    {
+                        i++;
+                        continue;
+                    }
+
+                    message.DateTime = Convert.ToDateTime(packet.SMSItems[0].Receive_EV[0].RecvTime);
+                    message.Number = Convert.ToInt32(packet.SMSItems[0].Receive_EV[0].Number);
+                    message.Text = packet.SMSItems[0].Receive_EV[0].Text;
+                    packet.SMSItems[0].Receive_EV = null;
+                    TrafficController.asyncData.Remove(packet);
+                }
+            }
+            return message;
+        }
+
+
+        public bool IsSyncChange_EV(List<XCTIP> packets)
+        {
+            foreach (var packet in packets)
+            {
+                if (packet.SyncItems == null || packet.SyncItems[0].Change_EV == null)
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Error na module SMS - wywoływany zawsze po wysłaniu ramki, bo zawsze otrzymujemy wiadomość i zawsze może ona zawierać error
+        /// </summary>
+        /// <returns></returns>
+        public bool SMSError(XCTIP packet)
+        {
+            if (packet == null || packet.SMSItems == null || packet.SMSItems[0].Answer == null)
+                return true;
+
+            if (packet.SMSItems[0].Answer[0].Error == null)
+            {
+                return false;
+            }
+            else
+            {
+                logger.Debug(packet.SMSItems[0].Answer[0].Error);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Error na module Sync - wywoływany zawsze po wysłaniu ramki, bo zawsze otrzymujemy wiadomość i zawsze może ona zawierać error
+        /// </summary>
+        /// <returns></returns>
+        public bool SyncError(XCTIP packet)
+        {
+            if (packet == null || packet.SyncItems == null || packet.SyncItems[0].Answer == null)
+                return true;
+
+            if (packet.SyncItems[0].Answer[0].Error == null)
+                return false;
+            else
+            {
+                logger.Debug(packet.SyncItems[0].Answer[0].Error);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Error na module Log - wywoływany zawsze po wysłaniu ramki, bo zawsze otrzymujemy wiadomość i zawsze może ona zawierać error
+        /// </summary>
+        /// <returns></returns>
+        public bool LogError(XCTIP packet)
+        {
+            if (packet == null || packet.LogItems == null || packet.LogItems[0].Answer == null)
+                return true;
+
+            if (packet.LogItems[0].Answer[0].Error == null)
+                return false;
+            else
+            {
+                logger.Debug(packet.LogItems[0].Answer[0].Error);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Error na module Status - wywoływany zawsze po wysłaniu ramki, bo zawsze otrzymujemy wiadomość i zawsze może ona zawierać error
+        /// </summary>
+        /// <returns></returns>
+        public bool StatusError(XCTIP packet)
+        {
+            if (packet == null || packet.StatusItems == null || packet.StatusItems[0].Answer == null)
+            {
+                logger.Debug("Format pakietu jest niezgodny");
+                return true;
+            }
+
+            if (packet.StatusItems[0].Answer[0].Error == null)
+                return false;
+            else
+            {
+                logger.Debug($"Error: {packet.StatusItems[0].Answer[0].Error}");
+                return true;
+            }
+        }
+
+        public async Task<List<XCTIP>> ParsePacket()
+        {
+            List<XCTIP> packets = new List<XCTIP>();
+            foreach (var item in await connection.GetFramesList())
+            {
+                var packet = ServiceXML.GenericDeserialize<XCTIP>(item);
+                packets.Add(packet);
+            }
+            return packets;
+        }
+    }
+}
